c1e09e1363da97e22a52cbf420b5096481e73488
b1tg <b1tg@protonmail.ch>
2019-01-08 01:20:30 +0800

    merge to one handle

--- src/main.rs
+++ src/main.rs
@@ -5,90 +5,68 @@ extern crate pretty_env_logger;
 extern crate subprocess;
 
 use std::fs::File;
-use std::path::Path;
 use std::io::Read;
+use std::path::Path;
 
-use std::io::{self, Write};
-use std::env;
-use hyper::{Client, Body, Method, Request};
 use hyper::rt::{self, Future, Stream};
+use hyper::{Body, Client, Method, Request};
+use std::env::args;
+use std::io::{self, Write};
 fn main() {
     pretty_env_logger::init();
 
-    let url = match env::args().nth(2) {
-        Some(url) => url,
-        None => {
-            println!("Usage: client <option> <url>");
-            return;
-        }
-    };
-
-    let method = match env::args().nth(1) {
-        Some(m) => m,
-        None => {
-            println!("Usage: client <option> <url>");
+    let (method, url) = match (args().nth(1), args().nth(2)) {
+        (Some(method), Some(url)) => (method, url),
+        _ => {
+            println!("Usage: ./postwomen <get|post|put|delete|..> <url>");
             return;
         }
     };
     let url = url.parse::<hyper::Uri>().unwrap();
+    rt::run(handle(method, url));
+}
 
-    if method == "post" {
-        rt::run(post_url(url));
+fn handle(method: String, url: hyper::Uri) -> impl Future<Item = (), Error = ()> {
+    let https = hyper_tls::HttpsConnector::new(4).unwrap();
+    let client = Client::builder().build::<_, Body>(https);
+
+    let mut req: Request<_>;
+    if method.as_str() == "post" || method.as_str() == "put" {
+        subprocess::Exec::cmd("vim")
+            .arg("#temp.json")
+            .join()
+            .unwrap();
+
+        let path = Path::new("#temp.json");
+        let mut file = File::open(&path).unwrap();
+        let mut json_s = String::new();
+        file.read_to_string(&mut json_s).unwrap();
+
+        req = Request::new(Body::from(json_s));
     } else {
-        rt::run(fetch_url(url));
+        req = Request::new(Body::from("{}"));
     }
-    
-}
-
-fn fetch_url(url: hyper::Uri) -> impl Future<Item=(), Error=()> {
-    let https = hyper_tls::HttpsConnector::new(4).unwrap();
-    let client = Client::builder()
-        .build::<_, Body>(https);
-
-    client.get(url)
-        .and_then(|res| {
-            println!("Response: {}", res.status());
-            res.into_body().for_each(|chunk| {
-                io::stdout().write_all(&chunk)
-                    .map_err(|e| panic!("example expects stdout is open, error={}",e))
-            })
-        })
-        .map(|_| {
-            println!("\n\nDone.");
-        })
-        .map_err(|err| {
-            eprintln!("Error {}",err);
-        })
-
-}
-
-fn post_url(url: hyper::Uri) -> impl Future<Item=(), Error=()> {
-    let https = hyper_tls::HttpsConnector::new(4).unwrap();
-    let client = Client::builder()
-        .build::<_, Body>(https);
-
-    subprocess::Exec::cmd("vim").arg("#temp.json").join().unwrap();
-
-    let path = Path::new("#temp.json");
-    let mut file = File::open(&path).unwrap();
-    let mut json_s = String::new();
-    file.read_to_string(&mut json_s).unwrap();   
-    //let json = r#"{"library":"hyper"}"#;
-
-    let mut req = Request::new(Body::from(json_s));
-    *req.method_mut() = Method::POST;
+    // *req.method_mut() = Method::POST;
+    *req.method_mut() = match method.as_str() {
+        "post" => Method::POST,
+        "put" => Method::PUT,
+        "delete" => Method::DELETE,
+        _ => Method::GET,
+    };
     *req.uri_mut() = url.clone();
     req.headers_mut().insert(
+        // need custom header
         hyper::header::CONTENT_TYPE,
-        hyper::header::HeaderValue::from_static("application/json")
+        hyper::header::HeaderValue::from_static("application/json"),
     );
-    
-    client.request(req)
+    client
+        .request(req)
         .and_then(|res| {
-            println!("Post: {}", res.status());
-            res.into_body().for_each(|chunk|{
-                io::stdout().write_all(&chunk)
-                    .map_err(|e| panic!("example expect stdout is open. err: {}",e))
+            println!("status: {}", res.status());
+            res.into_body().for_each(|chunk| {
+                io::stdout()
+                    .write_all(&chunk)
+                    .map_err(|e| panic!("expect stdout is open. err: {}", e))
             })
         })
         .map(|_| {
@@ -98,6 +76,3 @@ fn post_url(url: hyper::Uri) -> impl Future<Item=(), Error=()> {
             eprintln!("Error {}", err);
         })
 }
-
- 
-
